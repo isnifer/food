e5640a23a41fad2dad15b91ae012c53a9a5c19f1
Anton Kuznetsov <isnifer@gmail.com>
2020-01-03 21:05:17 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: '@detools/eslint-config',
+  globals: {
+    jest: false,
+    describe: false,
+    it: false,
+    test: false,
+    expect: false,
+    beforeAll: false,
+    beforeEach: false,
+    afterAll: false,
+    afterEach: false,
+  },
+}
