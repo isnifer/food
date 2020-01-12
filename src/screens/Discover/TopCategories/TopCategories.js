6934cf21d1221af2b0236223c7df46c4c81debda
Anton Kuznetsov <isnifer@gmail.com>
2020-01-12 23:58:02 +0300

    Added loaders for Discover Screen

--- src/screens/Discover/TopCategories/TopCategories.js
+++ src/screens/Discover/TopCategories/TopCategories.js
@@ -3,6 +3,8 @@ import { View, Text, TouchableOpacity, ScrollView, StyleSheet } from 'react-nati
 import PropTypes from 'prop-types'
 import { useQuery, gql } from '@apollo/client'
 import { withNavigation } from 'react-navigation'
+import { Placeholder, PlaceholderMedia, PlaceholderLine, Shine } from 'rn-placeholder'
+import { range } from 'lodash'
 import TopCategory from './TopCategory'
 
 const TOP_CATEGORIES = gql`
@@ -20,6 +22,41 @@ const TOP_CATEGORIES = gql`
   }
 `
 
+// eslint-disable-next-line
+function renderContent({ loading, data, handlePressCategory }) {
+  if (loading) {
+    return (
+      <View style={{ flexDirection: 'row', paddingTop: 16 }}>
+        {range(5).map(item => (
+          <View key={item} style={{ paddingLeft: 16 }}>
+            <Placeholder Animation={Shine} style={{ width: 88, marginLeft: item ? 88 : 0 }}>
+              <PlaceholderMedia style={{ width: 88, height: 88 }} />
+              <PlaceholderLine style={{ width: 44, marginTop: 9, height: 10 }} />
+              <PlaceholderLine style={{ width: 66, height: 10 }} />
+            </Placeholder>
+          </View>
+        ))}
+      </View>
+    )
+  }
+
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={styles.categoriesContainer}>
+      {data.categories.map((category, index) => (
+        <TopCategory
+          key={category.id}
+          category={category}
+          isFirst={index === 0}
+          onPress={handlePressCategory}
+        />
+      ))}
+    </ScrollView>
+  )
+}
+
 function Categories(props) {
   const { loading, error, data } = useQuery(TOP_CATEGORIES)
 
@@ -31,35 +68,19 @@ function Categories(props) {
     props.navigation.navigate('Restaurants', { categoryId: id, photo })
   }
 
-  if (loading) {
-    return <Text>Loading...</Text>
-  }
-
   if (error) {
     return <Text>{JSON.stringify(error, null, 2)}</Text>
   }
 
   return (
-    <View style={styles.container}>
+    <View>
       <View style={styles.headerContainer}>
         <Text style={styles.title}>Top Categories</Text>
         <TouchableOpacity onPress={handleShowAllCategories}>
           <Text style={styles.showAllButton}>Show all {'>'}</Text>
         </TouchableOpacity>
       </View>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        style={styles.categoriesContainer}>
-        {data.categories.map((category, index) => (
-          <TopCategory
-            key={category.id}
-            category={category}
-            isFirst={index === 0}
-            onPress={handlePressCategory}
-          />
-        ))}
-      </ScrollView>
+      {renderContent({ loading, data, handlePressCategory })}
     </View>
   )
 }
--- src/screens/Discover/TopPlaces/TopPlaces.js
+++ src/screens/Discover/TopPlaces/TopPlaces.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View, Text, ScrollView } from 'react-native'
 import { useQuery, gql } from '@apollo/client'
+import { Placeholder, PlaceholderMedia, PlaceholderLine, Shine } from 'rn-placeholder'
+import { range } from 'lodash'
 import TopPlace from './TopPlace'
 
 const TOP_PLACES = gql`
@@ -31,7 +33,20 @@ export default function TopPlaces() {
   const { loading, error, data } = useQuery(TOP_PLACES)
 
   if (loading) {
-    return <Text>Loading...</Text>
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        {range(3).map(item => (
+          <View key={item} style={{ paddingLeft: 16 }}>
+            <Placeholder Animation={Shine} style={{ width: 88, marginLeft: item ? 200 : 0 }}>
+              <PlaceholderMedia style={{ width: 200, height: 200 }} />
+              <PlaceholderLine style={{ width: 120, marginTop: 20, height: 10 }} />
+              <PlaceholderLine style={{ width: 150, marginTop: 20, height: 10 }} />
+              <PlaceholderLine style={{ width: 180, marginTop: 20, height: 10 }} />
+            </Placeholder>
+          </View>
+        ))}
+      </View>
+    )
   }
 
   if (error) {
